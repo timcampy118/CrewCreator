37afe8f0b094ec550ca22cfb50c7d20b60c0dcdf
Zheng Xiao <zwxxiao@gmail.com>
2020-01-05 07:05:47 +0000

    New client ID

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
+  # Whitelist console
+  config.web_console.whitelisted_ips = '66.96.207.244'
 end
