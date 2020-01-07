24efab02d1d119974a8f0cf7cd49f227ee35b61c
Timcampy118 <timothyn118@gmail.com>
2020-01-07 12:40:25 +0000

    change scrum time to datetime because heroku doesnt like time

--- db/migrate/20200107123834_change_scrum_time_to_date_time.rb
+++ db/migrate/20200107123834_change_scrum_time_to_date_time.rb
@@ -1,6 +1,5 @@
-class ChangeScrumTimeToDate < ActiveRecord::Migration[5.1]
+class ChangeScrumTimeToDateTime < ActiveRecord::Migration[5.1]
   def change
     change_column :teams, :scrum_time, :datetime 
   end
-
 end
